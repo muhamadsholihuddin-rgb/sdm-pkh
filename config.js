dfcc0908538c1b0c4985b9c48d4df0053d1a1195
muhamadsholihuddin-rgb <muhamadsholihuddin@gmail.com>
2026-09-06 08:50:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ISI DENGAN URL DEPLOY APPS SCRIPT KAMU (yang diakhiri /exec)
+// Ganti setiap kali kamu membuat "New deployment" baru di Apps Script.
+// Kalau kamu update kode lewat "Manage deployments > Edit > New version",
+// URL-nya TETAP SAMA dan file ini tidak perlu diubah.
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxhJKF9yDFEjrrm6zVKxy-r2AesYepA_dIm8EN2ClpYaq3UTenuOulfynxuvAO3r94AIA/exec";
